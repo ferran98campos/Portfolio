d2da8309ee8dd063627df36a12175d672e67a2b0
Ferran Campos <ferran98campos@gmail.com>
2023-02-14 04:00:58 +0100

    User Persona Finished
    The portfolio now allows the user to see my persona details

--- frontend/src/Components/footer/footer.js
+++ frontend/src/Components/footer/footer.js
@@ -1,8 +1,8 @@
 import { useState, useEffect, useRef, useLayoutEffect, useCallback} from 'react'
 import axios from "axios";
 import './footer.css';
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faArrowRight, faArrowLeft, faFile, faX, faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
@@ -58,6 +58,10 @@ const FooterComponent = () => {
         setPersonaShowing(true);
     }
 
+    const closePersonaLayout = () =>{
+        setPersonaShowing(false);
+    }
+
     const defineHelpList = () =>{
         var helpList = Array(4);
         for (var i=1; i<helpList.length+1;i++){
@@ -99,10 +103,63 @@ const FooterComponent = () => {
             </div>
 
             <div id="user-persona-background" style={{display: personaShowing==true ? "block" : "none"}}>
-                <div id="user-persona">
-                    <div id="user-persona-img">
+                <div id="user-persona" >
+                    <FontAwesomeIcon icon={faX} id="user-persona-close" onClick={closePersonaLayout}/>
+                    <div id="persona-image" style={{backgroundImage:'url(\'/img/persona_photo.png\')'}}>
+                        <div id="user-persona-quote">
+                            <p>"I believe the user should always be the cornerstone of software development"</p>
+                        </div>
                     </div>
+
                     <div id="user-persona-details">
+                        <div id="user-persona-name">
+                            <h2><b>FERRAN</b> CAMPOS LLOPART</h2>
+                            <p><i>Software Developer specializing in HCI</i></p>
+                        </div>
+                        <div id="user-persona-flex">
+                            <div id="user-persona-bio">
+                                <h3>About Me</h3>
+                                <hr></hr>
+                                <p>I am a <b>Interactive Media Technology Master’s student</b> at <i>KTH (Stockholm, Sweden)</i>, with a background in<b> Computer Science and Software Development</b> and some professional experience in the field. I am aware of how crucial it is to have the end users’ needs in mind when designing and building technology, I realized about this during my Bachelor's degree and I reaffirmed it along my job experience. My goal is to extend my professional career and deliver valuable software solutions to our users.</p>
+                            </div>
+                            <div id="user-persona-flex-mid">
+                                <div id="user-persona-skills">
+                                    <h3>Soft Skills</h3>
+                                    <hr></hr>
+                                    <ul>
+                                        <li>Organized</li>
+                                        <li>Fast Learner</li>
+                                        <li>Self-Reliance</li>
+                                        <li>Team Work</li>
+                                    </ul>
+                                </div>
+                                <div id="user-persona-languages">
+                                    <h3>Languages</h3>
+                                    <hr></hr>
+                                    <ul>
+                                        <li>English</li>
+                                        <li>Spanish</li>
+                                        <li>Catalan</li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div id="user-persona-flex-mid">
+                                <div id="user-persona-interests">
+                                    <h3>Interests</h3>
+                                    <hr></hr>
+                                    <p>I believe that my interests are varied. Aside from software and interaction, I am also a music enthusiast, reason why you may find some projects related to it, even though they might not have any relationship with software development or human interaction. </p>
+                                </div>
+                                <div id="user-persona-links">
+                                    <h3>Contact Me</h3>
+                                    <hr></hr>
+                                    <ul>
+                                        <li><FontAwesomeIcon icon={faEnvelope}/><b> Email: </b>ferran98campos@gmail.com</li>
+                                        <li><FontAwesomeIcon icon={faPhone}/><b> Phone (ES): </b>+34 674203066</li>
+                                        <li><FontAwesomeIcon icon={faPhone}/><b> Phone (SE): </b>+46 727809427</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -114,7 +171,9 @@ const FooterComponent = () => {
                 <div id="About" ref={personaButtonOpenRef}>More About Me</div>
                 <div id="Links" className="dropup">Social
                     <div className="dropup-content">
+                        <div><FontAwesomeIcon icon={faGithub}/><a href="https://github.com/ferran98campos?tab=repositories"> Github</a></div>
                         <div><FontAwesomeIcon icon={faLinkedin}/><a href="https://www.linkedin.com/in/ferran-campos-llopart/"> LinkedIn</a></div>
+                        <div><FontAwesomeIcon icon={faFile}/><a href="https://www.linkedin.com/in/ferran-campos-llopart/"> CV</a></div>
                     </div>
                 </div>
             </div>
---
+++ frontend/src/Components/project/project.js
@@ -0,0 +1,20 @@
+import { useState, useEffect, useRef, useLayoutEffect, useCallback} from 'react'
+import axios from "axios";
+import './project.css';
+
+
+const ProjectComponent = () => {
+
+    const [helpList, setHelpList] = useState(0)
+
+    useEffect(() => {
+    },[]);
+ 
+    return (
+        <div>
+
+        </div>
+    )
+}
+ 
+export default ProjectComponent
