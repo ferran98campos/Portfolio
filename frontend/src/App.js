5d98e296c284395a43937b5b18962a83752c4efd
Ferran Campos <ferran98campos@gmail.com>
2023-02-15 04:06:54 +0100

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NeuronsComponent from "./Components/neurons/neurons";
 import PathComponent from "./Components/path/pathComponent";
 import ProjectComponent from "./Components/project/project";
+import { HashRouter } from 'react-router-dom'
  
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
           <Routes>
             <Route path="/" exact element={ <NeuronsComponent/> }></Route>
             <Route path="/:id" exact element={ <ProjectComponent/> }></Route>
           </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
  
